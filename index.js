5fbb5ee008190d80ba8808ef8f21fcb982bdef89
Roger <rch.goldsnaker@gmail.com>
2021-11-13 12:29:24 -0300

    Changed where the images is uploaded

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
+/*const multer = require("multer");*/
+const { cloudinary } = require('./utils/cloudinary');
 const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+/*app.use("/images", express.static(path.join(__dirname, "/images")));*/
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -23,18 +24,33 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
   },
-});
+});*/
 
-const upload = multer({ storage: storage });
+/*const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
+});*/
+
+app.post('/api/upload', async (req, res) => {
+  try {
+      const fileStr = req.body.data;
+      const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+          upload_preset: 'dev_setups',
+      });
+      console.log(uploadResponse);
+      /*res.json({ msg: 'yaya' });*/
+      res.json(uploadResponse);
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ err: 'Something went wrong s4' });
+  }
 });
 
 app.use("/api/auth", authRoute);
@@ -42,6 +58,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Backend is running.");
 });
